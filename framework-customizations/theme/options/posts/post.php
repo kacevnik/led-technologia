3255849143f86b5b26071de79ec70ca1f14c62d8
kacevnik <kacevnik@yandex.ru>
2017-12-11 14:59:24 +0300

    Работа с Постом (Дом. Настройки)

--- framework-customizations/theme/options/posts/post.php
+++ framework-customizations/theme/options/posts/post.php
@@ -37,5 +37,96 @@ $options = array(
                 'extra_mime_types' => array( 'audio/x-aiff, aif aiff' )
             )
         )
+    ),
+
+    'product' =>array(
+        'type' => 'box',
+        'title' => __('Настройки карточки товара', '{domain}'),
+        'options' => array(
+            'kdv_on_settings_product' => array(
+                'type'  => 'switch',
+                'value' => '0',
+                'attr'  => array( 'class' => 'custom-class', 'data-foo' => 'bar' ),
+                'label' => __('Карточка товара?', '{domain}'),
+                'desc'  => __('Для того, чтобы страница отображалась как Карточка товара, необходимо включить эту настройку', '{domain}'),
+                'left-choice' => array(
+                    'value' => '0',
+                    'label' => __('Нет', '{domain}'),
+                ),
+                'right-choice' => array(
+                    'value' => '1',
+                    'label' => __('Да', '{domain}'),
+                ),
+            ),
+
+            'kdv_on_sitebar_product' => array(
+                'type'  => 'switch',
+                'value' => '0',
+                'attr'  => array( 'class' => 'custom-class', 'data-foo' => 'bar' ),
+                'label' => __('Включить сайтбар?', '{domain}'),
+                'desc'  => __('Для включения сайтбара в карточке товара используйте эту настройку', '{domain}'),
+                'left-choice' => array(
+                    'value' => '0',
+                    'label' => __('Нет', '{domain}'),
+                ),
+                'right-choice' => array(
+                    'value' => '1',
+                    'label' => __('Да', '{domain}'),
+                )
+            ),
+
+            'kdv_sub_title_product' => array(
+                'type'  => 'text',
+                'attr'  => array( 'class' => 'custom-class', 'data-foo' => 'bar' ),
+                'label' => __('Дополнительный заголовок', '{domain}')
+            ),
+
+            'add_img_product' => array(
+                'type'  => 'addable-box',
+                'attr'  => array( 'class' => 'custom-class', 'data-foo' => 'bar' ),
+                'label' => __('Добавить изображения', '{domain}'),
+                'desc'  => __('Добавить изображения для карточки товара', '{domain}'),
+                'box-options' => array(
+                    'kdv_img' => array(
+                        'type'  => 'upload',
+                        'value' => array(
+                            /*
+                            'attachment_id' => '9',
+                            'url' => '//site.com/wp-content/uploads/2014/02/whatever.jpg'
+                            */
+                            // if value is set in code, it is not considered and not used
+                            // because there is no sense to set hardcode attachment_id
+                        ),
+                        'attr'  => array( 'class' => 'custom-class', 'data-foo' => 'bar' ),
+                        'label' => __('Изображение', '{domain}'),
+                        'desc'  => __('', '{domain}'),
+                        'help'  => __('Загрузите изображение для карточки товара (разрешенные файлы для загрузки: jpg, png, gif)', '{domain}'),
+                        /**
+                         * If set to `true`, the option will allow to upload only images, and display a thumb of the selected one.
+                         * If set to `false`, the option will allow to upload any file from the media library.
+                         */
+                        'images_only' => true,
+                        /**
+                         * An array with allowed files extensions what will filter the media library and the upload files.
+                         */
+                        'files_ext' => array( 'jpg', 'png', 'gif' ),
+                        /**
+                         * An array with extra mime types that is not in the default array with mime types from the javascript Plupload library.
+                         * The format is: array( '<mime-type>, <ext1> <ext2> <ext2>' ).
+                         * For example: you set rar format to filter, but the filter ignore it , than you must set
+                         * the array with the next structure array( '.rar, rar' ) and it will solve the problem.
+                         */
+                        'extra_mime_types' => array( 'audio/x-aiff, aif aiff' )
+                    )
+                ),
+                'template' => 'Добавить картинку', // box title
+                'box-controls' => array( // buttons next to (x) remove box button
+                    'control-id' => '<small class="dashicons dashicons-smiley"></small>',
+                ),
+                'limit' => 0, // limit the number of boxes that can be added
+                'add-button-text' => __('Добавить', '{domain}'),
+                'sortable' => true,
+            )
+        )
     )
 );
--- single.php
+++ single.php
@@ -2,7 +2,13 @@
 <?php
 	if (defined( 'FW' )){
 		$kdv_post_background = fw_get_db_post_option(get_the_ID(), 'kdv_post_img');
+		$kdv_on_settings_product = fw_get_db_post_option(get_the_ID(), 'kdv_on_settings_product');
+		$kdv_on_sitebar_product = fw_get_db_post_option(get_the_ID(), 'kdv_on_sitebar_product');
+		$kdv_sub_title_product = fw_get_db_post_option(get_the_ID(), 'kdv_sub_title_product');
+		$add_img_product = fw_get_db_post_option(get_the_ID(), 'add_img_product');
 	}
+
+	if($kdv_on_settings_product == 0){
 ?>
 <section>
 	<div class="post_header" style="background: url('<?php echo $kdv_post_background['url']; ?>');">
@@ -26,4 +32,38 @@
 		</div>
 	</div>
 </section>
-<?php get_footer(); ?>
+<?php }else{  ?>
+<section>
+	<div class="post_header" style="background: url('<?php echo $kdv_post_background['url']; ?>');">
+		<div class="container">
+			<h1><?php the_title(); ?></h1>
+		</div>
+	</div>
+	<div class="container">
+		<div class="row">
+			<div class="<?php if($kdv_on_sitebar_product == 1){ ?>col-xs-12 col-sm-9 col-md-8<?php }else{ ?>post_product<?php } ?> post_body">
+				<?php if ( have_posts() ) while ( have_posts() ) : the_post(); // старт цикла ?>
+				<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>> <?php // контэйнер с классами и id ?>
+				<h2><?php the_title(); ?></h2>
+				<?php if($kdv_sub_title_product){ ?><div class="post_product_sub_title"><?php echo $kdv_sub_title_product; ?></div><?php } ?>
+				<div class="col-md-5">
+					<div class="post_product_img">
+						<?php echo $add_img_product[0]['kdv_img']['url']; ?>
+					</div>
+				</div>
+				<div class="col-md-7">
+					
+				</div>
+				</article>
+				<?php endwhile; // конец цикла ?>
+			</div>
+			<?php if($kdv_on_sitebar_product == 1){ ?>
+			<div class="col-xs-12 col-sm-3 col-md-4 page-sidebar">
+				<?php get_sidebar(); ?>
+			</div>
+			<?php } ?>
+		</div>
+	</div>
+</section>
+
+<?php } get_footer(); ?>
